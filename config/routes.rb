17ea97d1ab3722f5c4e58d0f9613f3db0537386a
Haruibos <sunada.381724@gmail.com>
2021-09-05 13:53:27 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # deviseを使用する際にURLとしてusersを含むことを示しています。
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  devise_for :users
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
+
+end
